b0edc3c26715425a45117e190aabbc279e429316
babodrogi <b0_baal@protonmail.com>
2020-12-27 12:32:12 +0100

    Empty project

---
+++ src/main/java/com/bb/abrishw/AbrisHwApplication.java
@@ -0,0 +1,13 @@
+package com.bb.abrishw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrisHwApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AbrisHwApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bb/abrishw/AbrisHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb.abrishw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrisHwApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
